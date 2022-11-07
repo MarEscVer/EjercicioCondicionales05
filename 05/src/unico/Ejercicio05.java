2cf584e7bc4f3798d4bdc8b17ba6709c980597a1
Maria <maria_escribanoverde@gmail.com>
2022-11-07 16:00:31 +0100

    Final-proyecto

---
+++ 05/src/unico/Ejercicio05.java
@@ -0,0 +1,29 @@
+package unico;
+
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		double a;
+		double b;
+		double x;
+		
+		System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+		System.out.print("Por favor, introduzca el valor de a: ");
+		a = Double.parseDouble(teclado.nextLine());
+		System.out.print("Ahora introduzca el valor de b: ");
+		b = Double.parseDouble(teclado.nextLine());
+		
+		if (a == 0) {
+			System.out.println("Esa ecuación no tiene solución real");
+		} else {
+			x = -b/a;
+			System.out.print("x = " + x);
+		}
+
+	}
+
+}
